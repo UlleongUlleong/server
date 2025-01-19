6124396c1c39ac19766f03eba570fd8ebfe638b8
subinggrae <subin000602@gmail.com>
2025-01-19 18:44:12 +0900

    feat: cors 설정

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,12 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.enableShutdownHooks();
+  app.enableCors({
+    origin: process.env.FRONTEND_ORIGIN,
+    methods: 'GET, POST, PUT, DELETE, PATCH',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
