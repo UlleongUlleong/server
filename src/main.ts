1f459420250a3be88baf948aa034a478658d3fc1
subinggrae <subin000602@gmail.com>
2025-01-23 10:47:41 +0900

    feat: 전역 오류 필터 기능

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalFilters(new GlobalExceptionFilter());
   app.enableShutdownHooks();
   app.enableCors({
     origin: process.env.FRONTEND_ORIGIN,
